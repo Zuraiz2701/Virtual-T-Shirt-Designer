6b265835f161a63bc8c5682b8315553113397c43
Zuraiz <zuraizmehar786@gmail.com>
2023-08-23 16:02:36 +0500

    Set Backdrop in Canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -10,7 +10,7 @@ const CanvasModel = () => {
 
     <Canvas
       shadows
-      camera={{ position: [0, 0, 0], fov: 50 }}
+      camera={{ position: [0, 0, 0], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
       className="w-full max-w-full h-full transition-all ease-in"
     >
